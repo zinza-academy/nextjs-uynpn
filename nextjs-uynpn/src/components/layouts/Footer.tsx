01ec2ca46cb266fe4363c37c168f10bf4594fd5c
phamngocuyn <uynpham123@gmail.com>
2024-06-28 14:31:08 +0700

    feat(homepage): create base footer

--- nextjs-uynpn/src/components/layouts/Footer.tsx
+++ nextjs-uynpn/src/components/layouts/Footer.tsx
@@ -1,7 +1,42 @@
+import React from "react";
+import { Box, Typography, Container, Grid } from "@mui/material";
+
 const Footer = () => {
     return (
-        <h1>Footer</h1>
-    );
+    <Box
+      component="footer"
+      sx={{
+        height: 256,
+        backgroundColor: "#2D2188",
+        color: "#FFFFFF",
+        display: "flex",
+      }}
+    >
+      <Container
+        sx={{
+            marginTop: '16px',
+            marginBottom: '16px',
+            marginLeft: '16px',
+            marginRight: '16px',
+          }}
+      >
+        <Grid container >
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6">About Us</Typography>
+            <Typography variant="body2">
+              Information about the company, mission, values, etc.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4} sx={{ ml: "auto" }}>
+            <Typography variant="h6">Follow Us</Typography>
+            <Typography variant="body2">
+              Links to social media profiles.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
 export default Footer
