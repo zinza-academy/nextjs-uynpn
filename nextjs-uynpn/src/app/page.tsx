6f7a8f2813fd98c99013a45d02568dd0878f9afa
phamngocuyn <uynpham123@gmail.com>
2024-06-29 18:16:55 +0700

    feat(homepage): Create header

--- nextjs-uynpn/src/app/page.tsx
+++ nextjs-uynpn/src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import Link from 'next/link';
 import React, { Suspense } from 'react';
+import Header from './component/layouts/header/page';
 
 const LazyHomePage = React.lazy(() => import('../app/dashboard/homepage/page'));
 
 export default function Home() {
   return (
     <main>
+      <Header />
+
       <Suspense fallback={<div>Loading...</div>}>
         <LazyHomePage />
       </Suspense>
