3f728c0bf733f9def95d6f00a2e7ed3302ccd9ee
phamngocuyn <uynpham123@gmail.com>
2024-07-03 17:14:52 +0700

    fix(homepage): using define type hook

--- nextjs-uynpn/src/app/component/layouts/header/page.tsx
+++ nextjs-uynpn/src/app/component/layouts/header/page.tsx
@@ -15,24 +15,23 @@ import Image from "next/image";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import EastIcon from "@mui/icons-material/East";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState, AppDispatch } from "@/lib/store";
+import { RootState, AppDispatch, useAppDispatch, useAppSelector } from "@/lib/store";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { fetchUser, clearUser } from "@/slice/userSlice";
 
 const Header = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [profileAnchorEl, setProfileAnchorEl] = useState<null | HTMLElement>(null);
-  const [showLoginButton, setShowLoginButton] = useState<boolean>(false); 
-  const token = useSelector((state: RootState) => state.login.token);
-  const dispatch: AppDispatch = useDispatch();
-  const { user, isLoading, error } = useSelector((state: RootState) => state.user);
+  const [showLoginButton, setShowLoginButton] = useState<boolean>(false);
+  const token = useAppSelector((state: RootState) => state.login.token);
+  const dispatch: AppDispatch = useAppDispatch ();
+  const { user, isLoading, error } = useAppSelector((state) => state.user);
 
   useEffect(() => {
     if (token && !user) {
       dispatch(fetchUser());
     } else {
-      setShowLoginButton(!token); 
+      setShowLoginButton(!token);
     }
   }, [token, user, dispatch]);
 
@@ -54,7 +53,7 @@ const Header = () => {
 
   const handleLogout = () => {
     dispatch(clearUser());
-    setShowLoginButton(true); 
+    setShowLoginButton(true);
     handleProfileMenuClose();
   };
 
--- nextjs-uynpn/src/lib/store.ts
+++ nextjs-uynpn/src/lib/store.ts
@@ -4,6 +4,7 @@ import forgotPasswordReducer from '../slice/forgotPasswordSlice';
 import loginReducer from '../slice/loginSlice';
 import registerReducer from '../slice/registerSlice';
 import userReducer from '@/slice/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const store = configureStore({
   reducer: {
@@ -16,5 +17,8 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
 
 export default store;
