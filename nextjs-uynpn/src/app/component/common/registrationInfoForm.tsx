25813cb06e74f9220111a3de3c378813a13191da
phamngocuyn <uynpham123@gmail.com>
2024-07-04 14:10:12 +0700

    feat(register-vaccine): create RegistrationInfoForm inject in RegisterVaccine

---
+++ nextjs-uynpn/src/app/component/common/registrationInfoForm.tsx
@@ -0,0 +1,229 @@
+import React from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+  FormHelperText,
+} from "@mui/material";
+import { Controller } from "react-hook-form";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+
+interface RegistrationInfoFormProps {
+  control: any;
+  errors: any;
+  setValue: Function;
+  priorityGroups: { id: number; name: string }[];
+  jobs: { id: number; name: string }[];
+  workPlaces: { id: number; name: string }[];
+  locations: { id: number; name: string }[];
+}
+
+const RegistrationInfoForm: React.FC<RegistrationInfoFormProps> = ({
+  control,
+  errors,
+  setValue,
+  priorityGroups,
+  jobs,
+  workPlaces,
+  locations,
+}) => {
+  return (
+    <Box sx={{ mt: 2 }}>
+      <Grid container spacing={2}>
+        <Typography sx={{ ml: 2 }} variant="body1">
+          1. Thông tin người tiêm
+        </Typography>
+        <Grid item xs={12} sm={12} md={12}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography sx={{ fontWeight: "regular" }}>
+                Nhóm ưu tiên <span>(*)</span>
+              </Typography>
+              <FormControl fullWidth>
+                <InputLabel id="priority-group-label">
+                  Nhóm ưu tiên
+                </InputLabel>
+                <Controller
+                  name="priorityGroup"
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="priority-group-label"
+                      id="priority-group"
+                      label="Nhóm ưu tiên"
+                    >
+                      {priorityGroups.map((group) => (
+                        <MenuItem key={group.id} value={group.name}>
+                          {group.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                />
+                {!!errors.priorityGroup && (
+                  <FormHelperText sx={{ color: "red" }}>
+                    {errors.priorityGroup.message}
+                  </FormHelperText>
+                )}
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography sx={{ fontWeight: "regular" }}>
+                Số thẻ BHYT
+              </Typography>
+              <FormControl fullWidth>
+                <InputLabel id="bhyt-label">Số thẻ BHYT</InputLabel>
+                <Controller
+                  name="bhyt"
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="bhyt-label"
+                      id="bhyt"
+                      label="Số thẻ BHYT"
+                    >
+                      <MenuItem value="123456789">123456789</MenuItem>
+                      <MenuItem value="987654321">987654321</MenuItem>
+                    </Select>
+                  )}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={12}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4} md={3}>
+              <Typography sx={{ fontWeight: "regular" }}>Nghề nghiệp</Typography>
+              <FormControl fullWidth>
+                <InputLabel id="job-label">Nghề nghiệp</InputLabel>
+                <Controller
+                  name="job"
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="job-label"
+                      id="job"
+                      label="Nghề nghiệp"
+                    >
+                      {jobs.map((job) => (
+                        <MenuItem key={job.id} value={job.name}>
+                          {job.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+              <Typography sx={{ fontWeight: "regular" }}>
+                Đơn vị công tác
+              </Typography>
+              <FormControl fullWidth>
+                <InputLabel id="workplace-label">Đơn vị công tác</InputLabel>
+                <Controller
+                  name="workplace"
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="workplace-label"
+                      id="workplace"
+                      label="Đơn vị công tác"
+                    >
+                      {workPlaces.map((place) => (
+                        <MenuItem key={place.id} value={place.name}>
+                          {place.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+              <Typography sx={{ fontWeight: "regular" }}>Địa điểm tiêm</Typography>
+              <FormControl fullWidth>
+                <InputLabel id="location-label">Địa điểm tiêm</InputLabel>
+                <Controller
+                  name="location"
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="location-label"
+                      id="location"
+                      label="Địa điểm tiêm"
+                    >
+                      {locations.map((location) => (
+                        <MenuItem key={location.id} value={location.name}>
+                          {location.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={12}>
+          <Typography variant="body1" sx={{ mb: 1, mt: 2 }}>
+            1. Thông tin đăng kí tiêm chủng
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4} md={3}>
+              <Typography sx={{ fontWeight: "regular" }}>Ngày tiêm</Typography>
+              <FormControl fullWidth>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label="Ngày tiêm"
+                    value={null} 
+                    onChange={(date) => setValue("vaccinationDate", date)}
+                    renderInput={(params: any) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+              <Typography sx={{ fontWeight: "regular" }}>Buổi tiêm</Typography>
+              <FormControl fullWidth>
+                <InputLabel id="vaccination-session-label">Buổi tiêm</InputLabel>
+                <Controller
+                  name="vaccinationSession"
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="vaccination-session-label"
+                      id="vaccination-session"
+                      label="Buổi tiêm"
+                    >
+                      <MenuItem value="Buổi sáng">Buổi sáng</MenuItem>
+                      <MenuItem value="Buổi chiều">Buổi chiều</MenuItem>
+                    </Select>
+                  )}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default RegistrationInfoForm;
--- nextjs-uynpn/src/app/dashboard/registervaccine/page.tsx
+++ nextjs-uynpn/src/app/dashboard/registervaccine/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 import React, { useState } from "react";
 import ItemStepper from "@/app/component/common/item-stepper";
 import Layout from "@/app/component/layouts/Layout";
@@ -7,24 +7,15 @@ import {
   Container,
   Grid,
   Typography,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Button,
-  TextField,
-  FormHelperText,
-  colors,
+  Button
 } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { RegisterVaccines } from "@/model/RegisterVaccines";
 import EastIcon from "@mui/icons-material/East";
 import WestIcon from "@mui/icons-material/West";
+import RegistrationInfoForm from "@/app/component/common/registrationInfoForm";
 
 // Validation schema using yup
 const schema = yup.object().shape({
@@ -83,7 +74,7 @@ const RegisterVaccine = () => {
   const {
     control,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { errors },
     setValue,
     reset,
   } = useForm({
@@ -97,8 +88,9 @@ const RegisterVaccine = () => {
   };
 
   const handleNextStep = () => {
-    setActiveStep((prevStep) => prevStep + 1);
-    console.log("step tiếp theo");
+    if (activeStep < steps.length - 1) {
+      setActiveStep((prevStep) => prevStep + 1);
+    }
   };
 
   const handleBackStep = () => {
@@ -134,21 +126,9 @@ const RegisterVaccine = () => {
             }}
             maxWidth="xl"
           >
-            <Grid
-              container
-              spacing={1}
-              justifyContent="center"
-              alignItems="center"
-              sx={{
-                mb: 2,
-                p: 2,
-                mt: 2,
-              }}
-            >
-              <Grid item xs={12} sm={12} md={12}>
-                <ItemStepper activeStep={activeStep} steps={steps} />
-              </Grid>
-            </Grid>
+            <Box sx={{ mt: 2 }}>
+              <ItemStepper activeStep={activeStep} steps={steps} />
+            </Box>
           </Container>
         </Box>
 
@@ -161,257 +141,56 @@ const RegisterVaccine = () => {
           }}
         >
           <Container maxWidth="xl" sx={{ marginBottom: 80 }}>
-            <Box sx={{ mt: 2 }}>
+            <RegistrationInfoForm
+              control={control}
+              errors={errors}
+              setValue={setValue}
+              priorityGroups={priorityGroups}
+              jobs={jobs}
+              workPlaces={workPlaces}
+              locations={locations}
+            />
+
+            <Grid item xs={12} sm={12} md={12} mt={4}>
               <Grid container spacing={2}>
-                <Typography sx={{ ml: 2 }} variant="body1">
-                  1. Thông tin người tiêm
-                </Typography>
-                <Grid item xs={12} sm={12} md={12}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6} md={3}>
-                      <Typography sx={{ fontWeight: "regular" }}>
-                        Nhóm ưu tiên <span>(*)</span>
-                      </Typography>
-                      <FormControl fullWidth>
-                        <InputLabel id="priority-group-label">
-                          Nhóm ưu tiên
-                        </InputLabel>
-                        <Controller
-                          name="priorityGroup"
-                          control={control}
-                          render={({ field }) => (
-                            <Select
-                              {...field}
-                              labelId="priority-group-label"
-                              id="priority-group"
-                              label="Nhóm ưu tiên"
-                            >
-                              {priorityGroups.map((group) => (
-                                <MenuItem key={group.id} value={group.name}>
-                                  {group.name}
-                                </MenuItem>
-                              ))}
-                            </Select>
-                          )}
-                        />
-                        {!!errors.priorityGroup && (
-                          <FormHelperText sx={{ color: "red" }}>
-                            {errors.priorityGroup.message}
-                          </FormHelperText>
-                        )}
-                      </FormControl>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                      <Typography sx={{ fontWeight: "regular" }}>
-                        Số thẻ BHYT
-                      </Typography>
-                      <FormControl fullWidth>
-                        <InputLabel id="bhyt-label">Số thẻ BHYT</InputLabel>
-                        <Controller
-                          name="bhyt"
-                          control={control}
-                          render={({ field }) => (
-                            <Select
-                              {...field}
-                              labelId="bhyt-label"
-                              id="bhyt"
-                              label="Số thẻ BHYT"
-                            >
-                              <MenuItem value="123456789">123456789</MenuItem>
-                              <MenuItem value="987654321">987654321</MenuItem>
-                            </Select>
-                          )}
-                        />
-                      </FormControl>
-                    </Grid>
-                  </Grid>
-                </Grid>
-
-                <Grid item xs={12} sm={12} md={12}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={4} md={3}>
-                      <Typography sx={{ fontWeight: "regular" }}>
-                        Nghề nghiệp
-                      </Typography>
-                      <FormControl fullWidth>
-                        <InputLabel id="job-label">Nghề nghiệp</InputLabel>
-                        <Controller
-                          name="job"
-                          control={control}
-                          render={({ field }) => (
-                            <Select
-                              {...field}
-                              labelId="job-label"
-                              id="job"
-                              label="Nghề nghiệp"
-                            >
-                              {jobs.map((job) => (
-                                <MenuItem key={job.id} value={job.name}>
-                                  {job.name}
-                                </MenuItem>
-                              ))}
-                            </Select>
-                          )}
-                        />
-                      </FormControl>
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={3}>
-                      <Typography sx={{ fontWeight: "regular" }}>
-                        Đơn vị công tác
-                      </Typography>
-                      <FormControl fullWidth>
-                        <InputLabel id="workplace-label">
-                          Đơn vị công tác
-                        </InputLabel>
-                        <Controller
-                          name="workplace"
-                          control={control}
-                          render={({ field }) => (
-                            <Select
-                              {...field}
-                              labelId="workplace-label"
-                              id="workplace"
-                              label="Đơn vị công tác"
-                            >
-                              {workPlaces.map((place) => (
-                                <MenuItem key={place.id} value={place.name}>
-                                  {place.name}
-                                </MenuItem>
-                              ))}
-                            </Select>
-                          )}
-                        />
-                      </FormControl>
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={3}>
-                      <Typography sx={{ fontWeight: "regular" }}>
-                        Địa điểm tiêm
-                      </Typography>
-                      <FormControl fullWidth>
-                        <InputLabel id="location-label">
-                          Địa điểm tiêm
-                        </InputLabel>
-                        <Controller
-                          name="location"
-                          control={control}
-                          render={({ field }) => (
-                            <Select
-                              {...field}
-                              labelId="location-label"
-                              id="location"
-                              label="Địa điểm tiêm"
-                            >
-                              {locations.map((location) => (
-                                <MenuItem
-                                  key={location.id}
-                                  value={location.name}
-                                >
-                                  {location.name}
-                                </MenuItem>
-                              ))}
-                            </Select>
-                          )}
-                        />
-                      </FormControl>
-                    </Grid>
-                  </Grid>
-                </Grid>
-
-
-                <Grid item xs={12} sm={12} md={12}>
-                <Typography variant="body1" sx={{ mb: 1, mt: 2 }}>
-                  1. Thông tin đăng kí tiêm chủng
-                </Typography>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={4} md={3}>
-                      <Typography sx={{ fontWeight: "regular" }}>
-                        Ngày tiêm
-                      </Typography>
-                      <FormControl fullWidth>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                          <DatePicker
-                            label="Ngày tiêm"
-                            value={null} // Giá trị này có thể cần điều chỉnh
-                            onChange={(date) =>
-                              setValue("vaccinationDate", date)
-                            }
-                            renderInput={(params: any) => (
-                              <TextField {...params} />
-                            )}
-                          />
-                        </LocalizationProvider>
-                      </FormControl>
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={3}>
-                      <Typography sx={{ fontWeight: "regular" }}>
-                        Buổi tiêm
-                      </Typography>
-                      <FormControl fullWidth>
-                        <InputLabel id="vaccination-session-label">
-                          Buổi tiêm
-                        </InputLabel>
-                        <Controller
-                          name="vaccinationSession"
-                          control={control}
-                          render={({ field }) => (
-                            <Select
-                              {...field}
-                              labelId="vaccination-session-label"
-                              id="vaccination-session"
-                              label="Buổi tiêm"
-                            >
-                              <MenuItem value="Buổi sáng">Buổi sáng</MenuItem>
-                              <MenuItem value="Buổi chiều">Buổi chiều</MenuItem>
-                            </Select>
-                          )}
-                        />
-                      </FormControl>
-                    </Grid>
-                  </Grid>
+                <Grid item xs={12} sm={6} md={12}>
+                  <Typography sx={{ fontWeight: "medium", color: "#D32F2F" }}>
+                    Lưu ý
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontWeight: "regular",
+                      color: "#D32F2F",
+                      ml: 1,
+                      mt: 2,
+                    }}
+                  >
+                    Việc đăng ký thông tin hoàn toàn bảo mật và phục vụ cho
+                    chiến dịch tiêm chủng Vắc xin COVID - 19
+                  </Typography>
+                  <Typography
+                    sx={{ fontWeight: "regular", color: "#D32F2F", ml: 1 }}
+                  >
+                    Xin vui lòng kiểm tra kỹ các thông tin bắt buộc(VD: Họ và
+                    tên, Ngày tháng năm sinh, Số điện thoại, Số CMND/CCCD/Mã
+                    định danh công dân/HC ...)
+                  </Typography>
+                  <Typography
+                    sx={{ fontWeight: "regular", color: "#D32F2F", ml: 1 }}
+                  >
+                    Bằng việc nhấn nút xác nhận, bạn hoàn toàn hiểu và đồng ý
+                    chịu trách nhiệm với các thông tin đã cung cấp.
+                  </Typography>
+                  <Typography
+                    sx={{ fontWeight: "regular", color: "#D32F2F", ml: 1 }}
+                  >
+                    Cá nhân/Tổ chức đăng ký thành công trên hệ thống sẽ được
+                    đưa vào danh sách đặt tiêm. Cơ sở y tế sẽ thông báo lịch
+                    tiêm khi có vắc xin và kế hoạch tiêm được phê duyệt. Trân
+                    trọng cảm ơn!
+                  </Typography>
                 </Grid>
               </Grid>
-            </Box>
-            
-            <Grid item xs={12} sm={12} md={12} mt={4}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6} md={12}>
-                    <Typography sx={{ fontWeight: "medium", color: "#D32F2F" }}>
-                      Lưu ý
-                    </Typography>
-                    <Typography
-                      sx={{
-                        fontWeight: "regular",
-                        color: "#D32F2F",
-                        ml: 1,
-                        mt: 2,
-                      }}
-                    >
-                      Việc đăng ký thông tin hoàn toàn bảo mật và phục vụ cho
-                      chiến dịch tiêm chủng Vắc xin COVID - 19
-                    </Typography>
-                    <Typography
-                      sx={{ fontWeight: "regular", color: "#D32F2F", ml: 1 }}
-                    >
-                      Xin vui lòng kiểm tra kỹ các thông tin bắt buộc(VD: Họ và
-                      tên, Ngày tháng năm sinh, Số điện thoại, Số CMND/CCCD/Mã
-                      định danh công dân/HC ...)
-                    </Typography>
-                    <Typography
-                      sx={{ fontWeight: "regular", color: "#D32F2F", ml: 1 }}
-                    >
-                      Bằng việc nhấn nút xác nhận, bạn hoàn toàn hiểu và đồng ý
-                      chịu trách nhiệm với các thông tin đã cung cấp.
-                    </Typography>
-                    <Typography
-                      sx={{ fontWeight: "regular", color: "#D32F2F", ml: 1 }}
-                    >
-                      Cá nhân/Tổ chức đăng ký thành công trên hệ thống sẽ được
-                      đưa vào danh sách đặt tiêm. Cơ sở y tế sẽ thông báo lịch
-                      tiêm khi có vắc xin và kế hoạch tiêm được phê duyệt. Trân
-                      trọng cảm ơn!
-                    </Typography>
-                  </Grid>
-                </Grid>
             </Grid>
 
             <Box sx={{ mt: 4, display: "flex", justifyContent: "center" }}>
@@ -423,9 +202,9 @@ const RegisterVaccine = () => {
                   backgroundColor: "#FFFFFF",
                   color: "#303F9F",
                   mt: 2,
-                  width: "130px", 
+                  width: "130px",
                   height: 36,
-                  borderRadius: "5px 5px 5px 0", 
+                  borderRadius: "5px 5px 5px 0",
                   "&:hover": {
                     backgroundColor: "#303F9F",
                     color: "#FFFFFF",
@@ -447,15 +226,14 @@ const RegisterVaccine = () => {
                   color: "#FFFFFF",
                   mt: 2,
                   ml: 2,
-                  width: "160px", 
+                  width: "160px",
                   height: 36,
-                  borderRadius: "5px 5px 5px 0", 
+                  borderRadius: "5px 5px 5px 0",
                   "&:hover": {
                     backgroundColor: "#FFFFFF",
                     color: "#303F9F",
                   },
                 }}
-                onClick={handleNextStep}
                 disabled={activeStep === steps.length - 1}
               >
                 Tiếp tục <EastIcon sx={{ color: "#FFFFFF", ml: 2 }} />
