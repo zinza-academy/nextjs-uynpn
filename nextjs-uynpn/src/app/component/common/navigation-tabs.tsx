4da83cb89dd30233013988bf63e7bb0280c959e0
phamngocuyn <uynpham123@gmail.com>
2024-07-08 13:50:02 +0700

    feat(account_vaccine_cerificate): Using Link in Navigation tab

--- nextjs-uynpn/src/app/component/common/navigation-tabs.tsx
+++ nextjs-uynpn/src/app/component/common/navigation-tabs.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Tabs, Tab, Divider } from "@mui/material";
+import Link from 'next/link';
 
 interface Props {
   value: number;
@@ -10,8 +11,8 @@ const NavigationTabs: React.FC<Props> = ({ value, onChange }) => {
   return (
     <Container sx={{ mb: 2, py: 2, height: 64 }} maxWidth="xl">
       <Tabs value={value} onChange={onChange} aria-label="navigation tabs">
-        <Tab label="Chứng nhận tiêm chủng" href="/dashboard/information-user" />
-        <Tab label="Kết quả đăng kí" href="/dashboard/vaccination-results" />
+        <Tab label="Chứng nhận tiêm chủng" component={Link} href="/dashboard/information-user" />
+        <Tab label="Kết quả đăng kí" component={Link} href="/dashboard/vaccination-results" />
         <Tab label="Tài khoản" />
       </Tabs>
       <Divider sx={{ height: 4, backgroundColor: "#EEEEEE" }} />
